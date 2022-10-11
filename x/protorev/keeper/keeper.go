6f565be6f0e2592817e3d82a2dbac7aceec8cbf5
davidterpay <david.terpay@gmail.com>
2022-10-11 13:36:34 -0500

    adding interface code

--- x/protorev/keeper/keeper.go
+++ x/protorev/keeper/keeper.go
@@ -3,33 +3,35 @@ package keeper
 import (
 	"fmt"
 
-	"github.com/tendermint/tendermint/libs/log"
-	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	"github.com/cosmos/cosmos-sdk/codec"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	"github.com/tendermint/tendermint/libs/log"
+
 	"github.com/osmosis-labs/osmosis/v12/x/protorev/types"
-	
 )
 
 type (
 	Keeper struct {
-		
-		cdc      	codec.BinaryCodec
-		storeKey 	storetypes.StoreKey
-		memKey   	storetypes.StoreKey
-		paramstore	paramtypes.Subspace
-		
+		cdc        codec.BinaryCodec
+		storeKey   storetypes.StoreKey
+		memKey     storetypes.StoreKey
+		paramstore paramtypes.Subspace
+
+		accountKeeper types.AccountKeeper
+		bankKeeper    types.BankKeeper
+		gammKeeper    types.GAMMKeeper
 	}
 )
 
 func NewKeeper(
-    cdc codec.BinaryCodec,
-    storeKey,
-    memKey storetypes.StoreKey,
+	cdc codec.BinaryCodec,
+	storeKey,
+	memKey storetypes.StoreKey,
 	ps paramtypes.Subspace,
-    
-    
+
 ) *Keeper {
 	// set KeyTable if it has not already been set
 	if !ps.HasKeyTable() {
@@ -37,12 +39,11 @@ func NewKeeper(
 	}
 
 	return &Keeper{
-		
-		cdc:      	cdc,
-		storeKey: 	storeKey,
-		memKey:   	memKey,
-		paramstore:	ps,
-		
+
+		cdc:        cdc,
+		storeKey:   storeKey,
+		memKey:     memKey,
+		paramstore: ps,
 	}
 }
 
--- x/protorev/keeper/protorev.go
+++ x/protorev/keeper/protorev.go
@@ -7,7 +7,15 @@ import (
 	"github.com/osmosis-labs/osmosis/v12/x/protorev/types"
 )
 
-func (k Keeper) GetNeedToArb(ctx sdk.Context) {
+func (k Keeper) GetNeedToArb(ctx sdk.Context) *types.NeedToArb {
 	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyNeedToArb)
 	value := store.Get(types.KeyNeedToArb)
+
+	if len(value) == 0 {
+		return nil
+	}
+
+	ret := &types.NeedToArb{}
+	ret.Unmarshal(value)
+	return ret
 }
