dd2ec064a2d8d1bf4a8058e0551f5f48f07907ae
Jeremy Liu <31809888+NotJeremyLiu@users.noreply.github.com>
2022-10-11 13:56:24 -0400

    add protorev keys - jeremy take

--- x/protorev/types/keys.go
+++ x/protorev/types/keys.go
@@ -1,5 +1,9 @@
 package types
 
+import (
+	"fmt"
+)
+
 const (
 	// ModuleName defines the module name
 	ModuleName = "protorev"
@@ -12,12 +16,31 @@ const (
 
 	// MemStoreKey defines the in-memory store key
 	MemStoreKey = "mem_protorev"
-
-    
 )
 
+var (
+	// ProtoRev Code
+	NeedToArbKey  = []byte("need_to_arb")
+	ArbDetailsKey = []byte("arb_details")
+)
 
+func GetTokenStoreKey(token string) []byte {
+	return []byte(fmt.Sprintf("token/%s", token))
+}
+
+func GetConnectedTokensPoolIDsStoreKey(tokenA, tokenB string) []byte {
+	// Compare tokenA and tokenB to see which one is alphabetically first
+	if tokenA < tokenB {
+		return []byte(fmt.Sprintf("connected_tokens/%s/%s", tokenA, tokenB))
+	} else {
+		return []byte(fmt.Sprintf("connected_tokens/%s/%s", tokenB, tokenA))
+	}
+}
+
+func GetPoolRoutesStoreKey(poolId uint64) []byte {
+	return []byte(fmt.Sprintf("pool_routes/%d", poolId))
+}
 
 func KeyPrefix(p string) []byte {
-    return []byte(p)
+	return []byte(p)
 }
